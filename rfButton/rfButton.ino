92957bbe09c2f85667bcaf81dfbc3bd755c99849
marcobombe <marcobombe@gmail.com>
2020-05-03 21:38:31 +0200

    5 Color complete support (button and receiver)

--- rfButton/rfButton.ino
+++ rfButton/rfButton.ino
@@ -15,28 +15,34 @@
 #define led 5
 
 #define BUTTON_BLUE
-//#define BUTTON_GREEN
-//#define BUTTON_RED
-//#define BUTTON_YELLOW
+//#define BUTTON_YELL
+//#define BUTTON_REDD
+//#define BUTTON_WHIT
+//#define BUTTON_GREE
 
 #ifdef BUTTON_BLUE
 const byte address[6] = "00001";
 const char text[] = "BUTTON_BLUE";
 #endif
 
-#ifdef BUTTON_GREEN
-const byte address[6] = "00001";
-const char text[] = "BUTTON_GREE";
-#endif
-
-#ifdef BUTTON_RED
-const byte address[6] = "00001";
-const char text[] = "BUTTON_RED";
-#endif
-
-#ifdef BUTTON_YELLOW
+#ifdef BUTTON_YELL
 const byte address[6] = "00001";
 const char text[] = "BUTTON_YELL";
+#endif
+
+#ifdef BUTTON_REDD
+const byte address[6] = "00001";
+const char text[] = "BUTTON_REDD";
+#endif
+
+#ifdef BUTTON_WHIT
+const byte address[6] = "00001";
+const char text[] = "BUTTON_WHIT";
+#endif
+
+#ifdef BUTTON_GREE
+const byte address[6] = "00001";
+const char text[] = "BUTTON_GREE";
 #endif
 
 RF24 radio(7, 8); // CE, CSN
@@ -61,18 +67,22 @@ void setup() {
   pinMode(button, INPUT_PULLUP);
   digitalWrite(led, LOW);
   digitalWrite(LED_BUILTIN, LOW);
+  
   #ifdef BUTTON_BLUE
   Serial.println("Blue Button started");
   #endif
-  #ifdef BUTTON_GREEN
-  Serial.println("Green Button started");
+  #ifdef BUTTON_YELL
+  Serial.println("Yellow Button started");
   #endif  
-  #ifdef BUTTON_RED
+  #ifdef BUTTON_REDD
   Serial.println("Red Button started");
   #endif 
-  #ifdef BUTTON_YELLOW
-  Serial.println("Yellow Button started");
+  #ifdef BUTTON_WHIT
+  Serial.println("White Button started");
   #endif
+  #ifdef BUTTON_GREE
+  Serial.println("Green Button started");
+  #endif  
 }
 
 void loop() {
--- rfReceiver/rfReceiver.ino
+++ rfReceiver/rfReceiver.ino
@@ -15,7 +15,15 @@ RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
 
+int led_yell = 10;
+int led_blue = 9;
+int led_redd = 4;
+int led_whit = 5;
+int led_gree = 6;
+
 int counter = 0;
+char text[32] = "";
+char winner[32] = "NO WIN";
 
 void setup() {
   Serial.begin(115200);
@@ -31,16 +39,43 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   Serial.println("Receiver started");
+
+  pinMode(led_yell, OUTPUT);
+  pinMode(led_blue, OUTPUT);
+  pinMode(led_redd, OUTPUT);
+  pinMode(led_whit, OUTPUT);
+  pinMode(led_gree, OUTPUT);      
+
+  digitalWrite(led_yell, LOW);
+  digitalWrite(led_blue, LOW);  
+  digitalWrite(led_redd, LOW);
+  digitalWrite(led_whit, LOW);
+  digitalWrite(led_gree, LOW);
 }
 
 void loop() {
   delay(100);
   if (radio.available()) {
-    char text[32] = "";
     radio.read(&text, sizeof(text));
     Serial.println("Received");
     Serial.println(text);
     Serial.println(counter);
+    if (!strcmp(winner, "NO WIN"))
+      strcpy(winner, text);
+    Serial.println("Winner is:");
+    Serial.println(winner);              
     counter ++;
+    
+    if (!strcmp(winner, "BUTTON_BLUE"))
+      digitalWrite(led_blue, HIGH);      
+    if (!strcmp(winner, "BUTTON_YELL"))
+      digitalWrite(led_yell, HIGH);   
+    if (!strcmp(winner, "BUTTON_REDD"))
+      digitalWrite(led_redd, HIGH);      
+    if (!strcmp(winner, "BUTTON_WHIT"))
+      digitalWrite(led_whit, HIGH);         
+    if (!strcmp(winner, "BUTTON_GREE"))
+      digitalWrite(led_gree, HIGH);  
+
   }
 }
